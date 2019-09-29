fefce0e8549efbdd64cb276ebeaba9396c874ab7
Michał <michalvichy@gmail.com>
2019-09-29 10:19:34 +0200

    Director name added

--- pages/[title].js
+++ pages/[title].js
@@ -1,36 +1,40 @@
-import React from 'react'
-import fetch from 'isomorphic-unfetch'
+import React from "react";
+import fetch from "isomorphic-unfetch";
 
 function IndexPage({ error, details }) {
   console.log(details);
   return (
     <>
-      {error ? <p>{error}</p> : (
+      {error ? (
+        <p>{error}</p>
+      ) : (
         <>
           <img src={details.Poster} />
-          <h2>{details.Title} ({details.Year})</h2>
+          <h2>
+            {details.Title} ({details.Year})
+          </h2>
+          <p>{details.Director}</p>
         </>
       )}
-
     </>
-  )
+  );
 }
 
 IndexPage.getInitialProps = async ({ query }) => {
   try {
     const res = await fetch(
       `${process.env.SERVER_URL}api/movie/${query.title}`
-    )
+    );
     if (res.ok) {
-      const { details } = await res.json()
-      return { details }
+      const { details } = await res.json();
+      return { details };
     }
-    throw new Error(`Failed to get response ${res.status}`)
+    throw new Error(`Failed to get response ${res.status}`);
   } catch (err) {
     return {
       error: err.message
-    }
+    };
   }
-}
+};
 
-export default IndexPage
+export default IndexPage;
